9352b48e4e376cceea62e47d11e1918a78505275
RylanFerrer <rylanferrer@gmail.com>
2020-01-28 21:26:22 -0800

    added board branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import "../Styles/main.css"
+import Board from './Boards'
+import "./Styles/main.css"
 function App() {
   return (
     <div className="App">
       <h1>Jira Board</h1>
+      <Board/>
     </div>
   );
 }
