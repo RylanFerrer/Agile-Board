bc57f262bf2e4cf3e801c9ed942b6999c10be45e
RylanFerrer <rylanferrer@gmail.com>
2020-02-04 17:07:07 -0800

    a

--- client/src/Components/Boards/Boards.js
+++ client/src/Components/Boards/Boards.js
@@ -1,79 +1,13 @@
 import React, {useState} from 'react';
 import initialData from '../Data/initial-data'
 import {DragDropContext, Droppable} from 'react-beautiful-dnd'
+import {onDragEnd} from "./Helpers/boardHelpers"
 import Column from './Column';
 const Boards = () => {
     const [data, setData] = useState({...initialData});
-    const onDragEnd = result => {
-        const {destination,source,draggableId,type} = result
-        if(!destination)
-        {
-            return
-        }
-        if(destination.draggableId === source.draggableId && destination.index === source.index) {
-            return
-        } 
-        if (type === "column") {
-            const newColumnOrder = Array.from(data.columnOrder)
-            newColumnOrder.splice(source.index, 1)
-            newColumnOrder.splice(destination.index,0, draggableId)
-
-            const newData = {
-                ...data, 
-                columnOrder: newColumnOrder
-            }
-            setData(newData)
-            return;
-        }
-        const start = data.columns[source.droppableId]
-        const finish = data.columns[destination.droppableId]
-        if(start ===finish) 
-        {
-            const newTaskIds =Array.from(start.taskIds)
-            newTaskIds.splice(source.index,1)
-            newTaskIds.splice(destination.index,0, draggableId);
-    
-            const newColumn = {
-                ...start,
-                taskIds: newTaskIds
-            }
-            const newData = {
-                ...data, 
-                columns: {
-                ...data.columns,
-                [newColumn.id]:newColumn
-                }
-            }   
-            setData(newData)
-            return;
-        }
-        ///Moving from one list to another
-        const startTasksIds = Array.from(start.taskIds)
-        startTasksIds.splice(source.index,1)
-        const newStart = {
-            ...start,
-            taskIds:startTasksIds
-        }
-        const finishTaskIds = Array.from(finish.taskIds)
-        finishTaskIds.splice(destination.index,0, draggableId);
-        const newFinish = {
-            ...finish,
-            taskIds:finishTaskIds
-        }
-        const newData = {
-            ...data, 
-            columns: {
-                ...data.columns,
-                [newStart.id]:newStart,
-                [newFinish.id]: newFinish, 
-            }
-        }
-        setData(newData)
-        return;
-    }
     return (
       
-            <DragDropContext onDragEnd = {onDragEnd}>
+            <DragDropContext onDragEnd = {result => onDragEnd(result,data,setData)}>
                 <Droppable droppableId = "all-columns" direction = "horizontal" type = "column">
                     {(provided) => (
                     <div className = "column__container"  ref = {provided.innerRef}{...provided.droppableProps}>
