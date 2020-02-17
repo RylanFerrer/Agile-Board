acb758280a1087cdb98e398a822fd6b5eb2d123d
RylanFerrer <rylanferrer@gmail.com>
2020-02-17 15:24:35 -0800

    added more options menu on lists

--- client/src/Components/Boards/Column.js
+++ client/src/Components/Boards/Column.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Task from './Task'
 import AddItem from './AddItem'
+import Options from "../OptionsMenu/Options"
 import {Droppable,Draggable} from 'react-beautiful-dnd'
 const Column = (props) => {
     const {column, tasks,index, projectId, reset, setModal} = props
@@ -9,7 +10,7 @@ const Column = (props) => {
         {(provided) => (
         <div className = "column"  ref = {provided.innerRef}  {...provided.draggableProps}>
             <h3  className = "column__text"{...provided.dragHandleProps}>{column.title.toUpperCase()}</h3>
-          
+            <Options projectId = {projectId} column = {column}/>
             <Droppable className = "test" droppableId ={column.id} type = "task">
                 {(provided) => (
                 <div  className = "task__container" ref = {provided.innerRef}  {...provided.droppableProps}>
---
+++ client/src/Components/OptionsMenu/OptionsMenu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+export default function OptionsMenu(props) {
+    const {isOpened} = props
+    const displayValue = isOpened ? 'block' : 'none'
+    console.log(props)
+    return (
+        <div style = {{display: displayValue}} className = "options__menu">
+            <h5 className = "options__menu-header">List Actions</h5>
+            <h5 onClick = {() => console.log('archive')} className = "options__menu-item">Archive List</h5>
+        </div>
+    )
+}
