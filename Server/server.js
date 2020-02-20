820e075321ac4910e7c297d516d2b67ce16384d4
RylanFerrer <rylanferrer@gmail.com>
2020-02-20 11:51:04 -0800

    added login code

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose');
 const app = express()
 
@@ -6,7 +7,9 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.connect('mongodb://localhost:27017/jiraDB', {useUnifiedTopology: true,useNewUrlParser: true});
 app.use(express.json())
+app.use(cookieParser())
 app.use('/api/projects', require('./Routes/API/projects'))
+app.use('/api/auth', require('./Routes/API/auth'))
 app.listen(PORT, (req,res) => {
     console.log(`Server is running on Port ${PORT}`)
 })
