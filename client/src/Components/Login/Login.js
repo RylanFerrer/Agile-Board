44bee996cda256aa9fa4aeb6b73b54de8c12772c
RylanFerrer <rylanferrer@gmail.com>
2020-03-17 15:29:16 -0700

    styled login page

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -5,6 +5,7 @@ import {useDispatch} from 'react-redux'
 import {enterUser} from "../Actions"
 export default function Login() {
     const [redirect, setRedirect] = useState(false)
+    const [warning, setWarning] = useState(false)
     const dispatch = useDispatch()
     const submitHandler = async(event) => {
         event.preventDefault()
@@ -13,26 +14,31 @@ export default function Login() {
             email: user.value,
             password: password.value
         }
-       const res =  await axios.post(`/api/auth/login`, data)
-       if(res.status === 200) {
+        try {
+            const res =  await axios.post(`/api/auth/login`, data)
             dispatch(enterUser(res.data))
             setRedirect(true) 
-       }
-        else {
-            alert('Error')
+        } catch (e) {
+            setWarning(true)
         }
+
+       
     }
     if(redirect === true) {
         return <Redirect to = "/"/>
     }
     return  (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit = {event => submitHandler(event)}>
-                <input type = 'email' name = "user" placeholder = "Email"></input>
-                <input  type = 'password' name = "password" placeholder = "Password"></input>
-                <button type = "submit">Login!</button>
-            </form>
+        <div className = "login">
+            <div className = "login__container">
+            <h3>Log In Below!</h3>
+                <form onSubmit = {event => submitHandler(event)}>
+                    <input required type = 'email' name = "user" placeholder = "Email"></input>
+                    <input  required type = 'password' name = "password" placeholder = "Password"></input>
+                    {warning ?  <p className = "login__warning">* Your Password or Email is typed in incorrectly</p> : ''}
+                    <button type = "submit">Login!</button>
+                </form>
+            </div>
+         
         </div>
     )
 }
